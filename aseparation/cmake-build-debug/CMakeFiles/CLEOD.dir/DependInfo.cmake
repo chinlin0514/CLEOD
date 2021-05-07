42db596077ecc4d3285efc3aa12c1c11f2f6ef8a
chinlin0514 <83757030+chinlin0514@users.noreply.github.com>
2021-05-07 17:25:09 +0800

    Add files via upload

---
+++ aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/taiwandd/aseparation/Backend/GarbageCollector.cpp" "/Users/taiwandd/aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/Backend/GarbageCollector.cpp.o"
+  "/Users/taiwandd/aseparation/Backend/VirtualMachine.cpp" "/Users/taiwandd/aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/Backend/VirtualMachine.cpp.o"
+  "/Users/taiwandd/aseparation/Frontend/Scanner.cpp" "/Users/taiwandd/aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/Frontend/Scanner.cpp.o"
+  "/Users/taiwandd/aseparation/Frontend/Token.cpp" "/Users/taiwandd/aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/Frontend/Token.cpp.o"
+  "/Users/taiwandd/aseparation/MiddleEnd/Bytecode.cpp" "/Users/taiwandd/aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/MiddleEnd/Bytecode.cpp.o"
+  "/Users/taiwandd/aseparation/MiddleEnd/Compiler.cpp" "/Users/taiwandd/aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/MiddleEnd/Compiler.cpp.o"
+  "/Users/taiwandd/aseparation/main.cpp" "/Users/taiwandd/aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ aseparation/cmake-build-debug/CMakeFiles/CLEOD.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLEOD.dir/main.cpp.o"
+  "CMakeFiles/CLEOD.dir/Frontend/Token.cpp.o"
+  "CMakeFiles/CLEOD.dir/Frontend/Scanner.cpp.o"
+  "CMakeFiles/CLEOD.dir/MiddleEnd/Bytecode.cpp.o"
+  "CMakeFiles/CLEOD.dir/MiddleEnd/Compiler.cpp.o"
+  "CMakeFiles/CLEOD.dir/Backend/VirtualMachine.cpp.o"
+  "CMakeFiles/CLEOD.dir/Backend/GarbageCollector.cpp.o"
+  "CLEOD.pdb"
+  "CLEOD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLEOD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
